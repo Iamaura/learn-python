17f0a62bea4c7d7caec637d8cf4e1f261aa595c3
iamaura68 <92586879+Iamaura@users.noreply.github.com>
2023-12-13 11:57:16 -0500

    Create kjFunFact_initials.py
    Fun Fact: I surpassed 100K streams on Spotify this past November!

---
+++ 1-hello-world/block-letters/kjFunFact_initials.py
@@ -0,0 +1,21 @@
+#Keleb, Jones: "I'm a recording artist "iamaura" who just surpassed 100K streams on spotify this past November (2024)"
+
+#Block Letters KJ
+letter_kj = """
+k    k         j
+k  k           j
+kk             j 
+k  k     j     j
+k    k    j j j   
+"""
+print(letter_kj)
+
+#Block Letter J
+letter_j = """
+      j    
+      j   
+      j              
+j     j     
+ j j j          
+"""
+print(letter_j)
